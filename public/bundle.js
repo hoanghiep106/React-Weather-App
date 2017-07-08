05c1f15dbe9d4da6c874d22d36e58584d7d5c9f0
Hoang Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2017-07-08 15:24:03 +0700

    Style example page

--- public/bundle.js
+++ public/bundle.js
@@ -25496,11 +25496,14 @@
 	        null,
 	        React.createElement(Nav, null),
 	        React.createElement(
-	            'h2',
-	            null,
-	            'Main component'
-	        ),
-	        props.children
+	            'div',
+	            { className: 'row' },
+	            React.createElement(
+	                'div',
+	                { className: 'columns medium-6 large-4 small-centered' },
+	                props.children
+	            )
+	        )
 	    );
 	};
 
@@ -27305,11 +27308,45 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'Examples page'
+	        React.createElement(
+	            'h1',
+	            { className: 'text-center' },
+	            'Examples page'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'Here are a few wxample locations to try out'
+	        ),
+	        React.createElement(
+	            'ol',
+	            null,
+	            React.createElement(
+	                'li',
+	                null,
+	                React.createElement(
+	                    Link,
+	                    { to: '/?location=Philadenphia' },
+	                    'Philadenphia, PA'
+	                )
+	            ),
+	            React.createElement(
+	                'li',
+	                null,
+	                React.createElement(
+	                    Link,
+	                    { to: '/?location=Hochiminh' },
+	                    'Hochiminh City'
+	                )
+	            )
+	        )
 	    );
 	};
 
--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,7 +1,19 @@
 var React = require('react')
+var {Link} = require('react-router');
 
 var Examples = props => (
-    <h3>Examples page</h3>
+    <div>
+        <h1 className="text-center">Examples page</h1>
+        <p>Here are a few wxample locations to try out</p>
+        <ol>
+            <li>
+                <Link to='/?location=Philadenphia'>Philadenphia, PA</Link>
+            </li>
+            <li>
+                <Link to='/?location=Hochiminh'>Hochiminh City</Link>
+            </li>
+        </ol>
+    </div>
 );
 
 module.exports = Examples;
